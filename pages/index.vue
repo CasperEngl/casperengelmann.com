d32d55d377b6d15b34149b5be9741a133790a547
Casper <me@casperengelmann.com>
2020-08-23 04:06:43 +0200

    changed max width of headline

--- pages/index.vue
+++ pages/index.vue
@@ -32,16 +32,14 @@
             :style="{
               background: `var(${code.color})`,
             }"
-            :class="{
-              [code.width]: true
-            }"
+            :class="code.width"
             class="h-4 rounded-full"
           />
         </div>
       </div>
       <div class="absolute top-0 inset-x-0 z-10 py-32 lg:py-40 text-gray-900" style="background: linear-gradient(150deg, rgba(0,0,0,0) 0%, currentColor 40%);">
         <div class="container">
-          <h1 class="max-w-4xl text-3xl lg:text-6xl font-bold text-white" v-html="home.headline" />
+          <h1 class="max-w-6xl text-3xl lg:text-6xl font-bold text-white" v-html="home.headline" />
         </div>
       </div>
     </section>
@@ -66,11 +64,11 @@ export default {
   async asyncData ({ $content }) {
     const home = await $content('home').fetch()
 
-    home.headline = wrapTextWithColor(home.headline, 'Vue', '#42b883')
+    home.headline = wrapTextWithColor(home.headline, 'Vue.js', '#42b883')
     home.headline = wrapTextWithColor(home.headline, 'React', '#00d8ff')
     home.headline = wrapTextWithColor(home.headline, 'TypeScript', '#3178c6')
     home.headline = wrapTextWithColor(home.headline, 'PHP', '#8892be')
-    home.headline = wrapTextWithColor(home.headline, 'Node', '#6cc24a')
+    home.headline = wrapTextWithColor(home.headline, 'Node.js', '#6cc24a')
 
     return {
       home,
