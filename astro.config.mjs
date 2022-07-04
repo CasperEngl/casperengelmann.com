69e36c5684fe77d967a94a8004fe05b19993774a
Casper Engelmann <me@casperengelmann.com>
2022-07-04 10:42:39 +0200

    rats

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 import { defineConfig } from 'astro/config'
-import vercel from '@astrojs/vercel/static'
+import adapter from '@astrojs/deno'
 
 import react from '@astrojs/react'
 import tailwind from '@astrojs/tailwind'
-import partytown from '@astrojs/partytown'
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: vercel(),
-  integrations: [react(), tailwind(), partytown()],
+  adapter: adapter(),
+  integrations: [react(), tailwind()],
 })
