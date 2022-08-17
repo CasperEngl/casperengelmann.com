4a6fba15271113d8634869254b40b509c571ddea
Casper Engelmann <me@casperengelmann.com>
2022-08-17 13:03:42 +0200

    I did it for the lulz!

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
-import { defineConfig } from 'astro/config'
 import adapter from '@astrojs/deno'
 import react from '@astrojs/react'
 import tailwind from '@astrojs/tailwind'
-
-import partytown from '@astrojs/partytown'
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: adapter(),
-  integrations: [react(), tailwind(), partytown()],
+	output: 'server',
+	adapter: adapter(),
+	integrations: [react(), tailwind()],
 })
