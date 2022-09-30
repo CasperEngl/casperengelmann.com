670cdabbd827b55cacba19c8246cfe618de91570
Casper Engelmann <me@casperengelmann.com>
2022-09-30 23:30:39 +0200

    Popping stash

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
-import adapter from '@astrojs/deno'
 import mdx from '@astrojs/mdx'
+import netlify from '@astrojs/netlify/edge-functions'
 import solid from '@astrojs/solid-js'
 import tailwind from '@astrojs/tailwind'
 import { defineConfig } from 'astro/config'
@@ -7,6 +7,6 @@ import { defineConfig } from 'astro/config'
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: adapter(),
+  adapter: netlify(),
   integrations: [tailwind(), solid(), mdx()],
 })
