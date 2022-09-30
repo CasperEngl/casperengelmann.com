ba93fd808873d07a75581f3f31bc43b969868eee
Casper Engelmann <me@casperengelmann.com>
2022-10-01 00:17:00 +0200

    Ok, 5am, it works.  For real.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
-import cloudflare from '@astrojs/cloudflare'
-import mdx from '@astrojs/mdx'
-import solid from '@astrojs/solid-js'
-import tailwind from '@astrojs/tailwind'
-import { defineConfig } from 'astro/config'
+import cloudflare from '@astrojs/cloudflare';
+import mdx from '@astrojs/mdx';
+import solid from '@astrojs/solid-js';
+import tailwind from '@astrojs/tailwind';
+import { defineConfig } from 'astro/config';
+
+import deno from "@astrojs/deno";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: cloudflare(),
-  integrations: [tailwind(), solid(), mdx()],
-})
+  adapter: deno(),
+  integrations: [tailwind(), solid(), mdx()]
+});
