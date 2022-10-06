5ff27af1e7b6640c8b1d174cbcd8d4d43e51bc1d
Casper Engelmann <me@casperengelmann.com>
2022-10-06 03:16:41 +0200

    If it's stupid and it works, it ain't stupid

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import mdx from '@astrojs/mdx'
 import solid from '@astrojs/solid-js'
 import tailwind from '@astrojs/tailwind'
-import vercel from '@astrojs/vercel/serverless'
 import { defineConfig } from 'astro/config'
+
+import deno from "@astrojs/deno"
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: vercel(),
-  integrations: [tailwind(), solid(), mdx()],
+  adapter: deno(),
+  integrations: [tailwind(), solid(), mdx()]
 })
