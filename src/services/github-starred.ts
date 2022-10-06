ebe23e36248ff897c87914c11dad621ea67f73b2
Casper Engelmann <me@casperengelmann.com>
2022-10-06 03:51:51 +0200

    syntax

--- src/services/github-starred.ts
+++ src/services/github-starred.ts
@@ -1,49 +1,53 @@
+import ms from 'ms'
+import { upstashRequest } from '~/services/upstash'
+
 interface Repo {
   html_url: string
   full_name: string
   private?: boolean
 }
 
+type CacheResult = {
+  expires: string
+  repos: Repo[]
+}
+
 export async function getStarredRepos() {
-  // const cache = await upstashRequest<CacheResult>('/get/my-starred-repos')
+  const cache = await upstashRequest<CacheResult>('/get/my-starred-repos')
 
-  // if (cache) {
-  //   console.log('My starred repos: cache hit')
+  if (cache) {
+    console.log('My starred repos: cache hit')
 
-  //   return cache.repos
-  // }
-
-  // console.log('My starred repos: cache miss')
-
-  try {
-    const response = await fetch(
-      'https://api.github.com/users/casperengl/starred?per_page=10',
-      {
-        headers: {
-          Authorization: `token ${import.meta.env.GITHUB_API_KEY}`,
-        },
-      }
-    )
-
-    const json = await response.json()
-    // const expires = ms('1 hour') / 1000 // ms to seconds
-    const repos: Repo[] = json
-      .filter((repo) => !repo.private)
-      .map((repo) => ({
-        html_url: repo.html_url,
-        full_name: repo.full_name,
-      }))
-
-    // await upstashRequest(`/set/my-starred-repos?EX=${expires}`, {
-    //   method: 'POST',
-    //   body: JSON.stringify({
-    //     expiresAt: new Date(Date.now() + ms('1 hour')).toISOString(),
-    //     repos,
-    //   }),
-    // })
-
-    return repos
-  } catch (error) {
-    console.error(error)
+    return cache.repos
   }
+
+  console.log('My starred repos: cache miss')
+
+  const response = await fetch(
+    'https://api.github.com/users/casperengl/starred?per_page=10',
+    {
+      headers: {
+        Authorization: `token ${import.meta.env.GITHUB_API_KEY}`,
+      },
+    }
+  )
+
+  const json = await response.json()
+  const expires = ms('1 hour') / 1000 // ms to seconds
+  const repos: Repo[] = json
+    .filter((repo) => !repo.private)
+    .map((repo) => ({
+      html_url: repo.html_url,
+      full_name: repo.full_name,
+    }))
+
+  await upstashRequest(`/set/my-starred-repos?EX=${expires}`, {
+    method: 'POST',
+    body: JSON.stringify({
+      expiresAt: new Date(Date.now() + ms('1 hour')).toISOString(),
+      repos,
+    }),
+  })
+
+  return repos
 }
