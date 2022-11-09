ad180d571b87952c13d898d33ed90c3d3a3ada89
Casper Engelmann <cen@makeinfluence.com>
2022-11-09 23:07:52 +0100

    Improvements

--- src/services/redis.ts
+++ src/services/redis.ts
@@ -1,5 +1,10 @@
 import { Redis } from '@upstash/redis'
 
+console.log({
+  url: import.meta.env.UPSTASH_REDIS_REST_URL,
+  token: import.meta.env.UPSTASH_REDIS_REST_TOKEN,
+})
+
 export const redis = new Redis({
   url: import.meta.env.UPSTASH_REDIS_REST_URL,
   token: import.meta.env.UPSTASH_REDIS_REST_TOKEN,
