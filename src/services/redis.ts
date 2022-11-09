4144a51310268a50ef8d1467ffd2040b90bfe56b
Casper Engelmann <cen@makeinfluence.com>
2022-11-09 23:35:23 +0100

    Remove debug log

--- src/services/redis.ts
+++ src/services/redis.ts
@@ -1,10 +1,5 @@
 import { Redis } from '@upstash/redis'
 
-console.log({
-  url: Deno.env.get('UPSTASH_REDIS_REST_URL'),
-  token: Deno.env.get('UPSTASH_REDIS_REST_TOKEN'),
-})
-
 export const redis = new Redis({
   url: Deno.env.get('UPSTASH_REDIS_REST_URL'),
   token: Deno.env.get('UPSTASH_REDIS_REST_TOKEN'),
