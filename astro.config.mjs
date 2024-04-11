ed7b1eed51e078737dc3fb9d8985e755ba1c0501
Casper Engelmann <c.engelmann@custimy.io>
2024-04-12 01:40:11 +0200

    Update Astro config and dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import mdx from '@astrojs/mdx'
+import node from '@astrojs/node'
 import solid from '@astrojs/solid-js'
 import tailwind from '@astrojs/tailwind'
 import { defineConfig } from 'astro/config'
 
-import deno from '@astrojs/deno'
-
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: deno(),
+  adapter: node({
+    mode: 'standalone',
+  }),
   integrations: [tailwind(), solid(), mdx()],
 })
