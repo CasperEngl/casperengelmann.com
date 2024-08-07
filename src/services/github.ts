17f095529ba835495cf4235be679614af5f50086
Casper Engelmann <c.engelmann@custimy.io>
2024-08-07 20:29:58 +0200

    Refactor getStarredRepos to filter and parse JSON

--- src/services/github.ts
+++ src/services/github.ts
@@ -17,7 +17,7 @@ const cacheResultSchema = z.object({
   repos: z.array(repoSchema),
 })
 
-export async function setStarredRepos() {
+export async function getStarredRepos() {
   const response = await fetch(
     'https://api.github.com/users/casperengl/starred?per_page=10',
     {
@@ -27,56 +27,20 @@ export async function setStarredRepos() {
 
   const json = await response.json()
 
-  const repos = z.array(repoSchema).safeParse(json)
+  const repos = z.array(repoSchema).parse(json)
 
-  if (repos.success) {
-    const transformedRepos = repos.data
-      .filter((repo) => !repo.private)
-      .map((repo) => ({
-        html_url: repo.html_url,
-        full_name: repo.full_name,
-      }))
+  const filteredRepos = repos.filter((repo) => !repo.private)
 
-    const expires = ms('1 hour')
-    const expiryDate = new Date(Date.now() + expires)
-    const body: z.infer<typeof cacheResultSchema> = {
-      expiresAt: new Intl.DateTimeFormat('en-GB', {
-        dateStyle: 'short',
-        timeStyle: 'medium',
-        timeZone: 'Europe/Copenhagen',
-      }).format(expiryDate),
-      repos: transformedRepos,
-    }
+  const expires = ms('1 hour')
+  const expiryDate = new Date(Date.now() + expires)
+  const body = {
+    expiresAt: new Intl.DateTimeFormat('en-GB', {
+      dateStyle: 'short',
+      timeStyle: 'medium',
+      timeZone: 'Europe/Copenhagen',
+    }).format(expiryDate),
+    repos: filteredRepos,
+  } satisfies z.infer<typeof cacheResultSchema>
 
-    // await redis.set('my-starred-repos', body, {
-    //   ex: expires / 1000, // ms to seconds
-    // })
-
-    return transformedRepos
-  }
-
-  console.error('Failed to parse repos', repos.error)
-}
-
-export async function getStarredRepos() {
-  try {
-    // const cachedRepos = await redis.get('my-starred-repos').catch((error) => {
-    //   console.error('Error getting starred repos from cache', error)
-    // })
-    // const cache = cacheResultSchema.safeParse(cachedRepos || {})
-
-    // if (cache.success) {
-    //   console.log('My starred repos: cache hit')
-
-    //   return cache.data.repos
-    // }
-
-    // console.log('Failed to parse cache', cache.error)
-
-    console.log('My starred repos: cache miss')
-
-    return setStarredRepos()
-  } catch (error) {
-    console.error(error)
-  }
+  return body.repos
 }
