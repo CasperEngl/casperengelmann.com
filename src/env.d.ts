3e0924940941ddd2f777afb46f73eadf329da750
Casper Engelmann <c.engelmann@custimy.io>
2024-08-07 16:56:58 +0200

    Add AstroJS runtime configuration and bind a KV namespace

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,10 +1,12 @@
-interface ImportMetaEnv {
-  readonly DENO_DEPLOY_TOKEN: string
-  readonly GITHUB_API_KEY: string
-  readonly UPSTASH_REDIS_REST_TOKEN: string
-  readonly UPSTASH_REDIS_REST_URL: string
+/// <reference types="astro/client" />
+
+type KVNamespace = import('@cloudflare/workers-types').KVNamespace
+type ENV = {
+  MY_KV: KVNamespace
 }
 
-interface ImportMeta {
-  readonly env: ImportMetaEnv
+// use a default runtime configuration (advanced mode).
+type Runtime = import('@astrojs/cloudflare').Runtime<ENV>
+declare namespace App {
+  interface Locals extends Runtime {}
 }
